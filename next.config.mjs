cacd34f6ec42b499495f6a17ad376fe9d36f1b66
UTHRATHITHAN <m.uthrathithan@gmail.com>
2024-03-25 19:51:59 +0530

    Added Domain URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        DOMAIN_NAME:"localhost:3000/"
+        DOMAIN_NAME:"https://url-shortener-theta-weld.vercel.app/"
     }
 };
 
